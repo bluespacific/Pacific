7d34db3714e98e98997e965b541f2d84abf82561
Prashant Ranjan <prashantat@yahoo.com>
2017-05-28 18:59:54 -0400

    path update

--- concourse-spring-boot/concourse/buildactivity/buildscript.sh
+++ concourse-spring-boot/concourse/buildactivity/buildscript.sh
@@ -8,8 +8,10 @@ echo "====Build Started====="
 pwd
 ./hello-springboot-application/concourse-spring-boot/gradlew build
 echo "====Build Completed Successfully====="
+cd hello-springboot-application
 pwd
 #all input resources are copied to concourse-spring-boot-output directory within container
-cp ./hello-springboot-application/concourse-spring-boot/build/libs/concourse-spring-boot*.jar ../concourse-spring-boot-output
-cp ./hello-springboot-application/concourse-spring-boot/manifest.yml ../concourse-spring-boot-output
+cp concourse-spring-boot/build/libs/concourse-spring-boot*.jar ../concourse-spring-boot-output
+echo "jar copied success"
+cp ./manifest.yml ../concourse-spring-boot-output
 echo "=======Copied to Output folder======"
