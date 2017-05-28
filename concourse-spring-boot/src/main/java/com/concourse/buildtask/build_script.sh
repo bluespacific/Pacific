4bdce72d127e7ed1f5f3eda9c8f6c2dede31cc9d
Prashant Ranjan <prashantat@yahoo.com>
2017-05-28 01:28:52 -0400

    changes for task

---
+++ concourse-spring-boot/src/main/java/com/concourse/buildtask/build_script.sh
@@ -0,0 +1,8 @@
+echo "====Build Started====="
+/.gradlew build
+echo "====Build Completed Successfully====="
+pwd
+#all input resources are copied to concourse-spring-boot-output directory within container
+cp ./build/libs/concourse-spring-boot*.jar ../concourse-spring-boot-output
+cp ./manifest.yml ../concourse-spring-boot-output
+echo "=======Copied to Output folder======"
