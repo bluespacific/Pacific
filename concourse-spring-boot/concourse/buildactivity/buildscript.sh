1b3ff071a859d2f76d3e11c217bcb3ab7313031b
Prashant Ranjan <prashantat@yahoo.com>
2017-05-28 18:50:13 -0400

    fixed path of resources

--- concourse-spring-boot/concourse/buildactivity/buildscript.sh
+++ concourse-spring-boot/concourse/buildactivity/buildscript.sh
@@ -6,10 +6,10 @@ export TERM=${TERM:-dumb}
 
 echo "====Build Started====="
 pwd
-../gradlew build
+./hello-springboot-application/concourse-spring-boot/gradlew build
 echo "====Build Completed Successfully====="
 pwd
 #all input resources are copied to concourse-spring-boot-output directory within container
-cp ./build/libs/concourse-spring-boot*.jar ../concourse-spring-boot-output
-cp ./manifest.yml ../concourse-spring-boot-output
+cp ./hello-springboot-application/concourse-spring-boot/build/libs/concourse-spring-boot*.jar ../concourse-spring-boot-output
+cp ./hello-springboot-application/concourse-spring-boot/manifest.yml ../concourse-spring-boot-output
 echo "=======Copied to Output folder======"
