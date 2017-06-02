78816a6a66600675c05c50b7ff7017d0ccbc524d
Prashant Ranjan <prashantat@yahoo.com>
2017-06-01 20:47:07 -0400

    commit path sh change

--- concourse-spring-boot/concourse/buildactivity/buildscript.sh
+++ concourse-spring-boot/concourse/buildactivity/buildscript.sh
@@ -5,16 +5,22 @@ set -x # print commands
 export TERM=${TERM:-dumb}
 
 echo "====Build Started====="
+
 pwd
-./hello-springboot-application/concourse-spring-boot/gradlew build
-echo "====Build Completed Successfully====="
 cd hello-springboot-application
+./concourse-spring-boot/gradlew build
+ls -l
+echo "====Build Completed Successfully====="
+#cd hello-springboot-application
 ls -l
 pwd
-#cd concourse-spring-boot
+cd concourse-spring-boot
+echo "In concourse-spring-boot"
+ls -l
+
 #pwd
 #all input resources are copied to concourse-spring-boot-output directory within container #./concourse-spring-boot-output
-cp ./concourse-spring-boot/build/libs/concourse-spring-boot*.jar ../concourse-spring-boot-output
+cp ./build/libs/concourse-spring-boot*.jar ../concourse-spring-boot-output
 echo "jar copied success"
-cp ./concourse-spring-boot/manifest.yml ../concourse-spring-boot-output
+cp ./manifest.yml ../concourse-spring-boot-output
 echo "=======Copied to Output folder======"
