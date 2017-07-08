9207dcbf09850ef96163d785636a1f374fbd0a71
Prashant Ranjan <prashantat@yahoo.com>
2017-07-08 00:17:23 -0400

    changes for build test task

--- concourse-spring-boot/concourse/buildactivity/buildtestscript.sh
+++ concourse-spring-boot/concourse/buildactivity/buildtestscript.sh
@@ -6,10 +6,12 @@ export TERM=${TERM:-dumb}
 
 echo "====Build Started====="
 pwd
+cd hello-springboot-application
 ls -l
-./hello-springboot-application/concourse-spring-boot/gradlew build test --info
+cd concourse-spring-boot
+./gradlew build test --info
 echo "====Test Completed Successfully====="
-cd hello-springboot-application/concourse-spring-boot/build/classes/test/com/application/concourse/concourse
-ls -l
-pwd
+#cd hello-springboot-application/concourse-spring-boot/build/classes/test/com/application/concourse/concourse
+#ls -l
+#pwd
 echo "=======Exit Test Task======"
